c7c18227b1cbddb72e18e00514eb0cbaf2d87337
Jana <jana.vadillo1@gmail.com>
2024-12-15 11:31:15 -0600

    updated UI

--- src/main/java/edu/grinnell/csc207/main/BlockChainUI.java
+++ src/main/java/edu/grinnell/csc207/main/BlockChainUI.java
@@ -20,15 +20,41 @@ public class BlockChainUI {
   private static void printInstructions(PrintWriter pen) {
     pen.println("""
       Valid commands:
-      mine: discovers the nonce for a given transaction
-      append: appends a new block onto the end of the chain
-      remove: removes the last block from the end of the chain
-      check: checks that the block chain is valid
-      users: prints a list of users
-      balance: finds a user's balance
-      help: prints this list of commands
-      quit: quits the program""");
+        mine: discovers the nonce for a given transaction
+        append: appends a new block onto the end of the chain
+        remove: removes the last block from the end of the chain
+        check: checks that the block chain is valid
+        users: prints a list of users
+        balance: finds a user's balance
+        help: prints this list of commands
+        quit: quits the program""");
   } // printInstructions(PrintWriter)
+
+  private static Transaction promptTransaction(PrintWriter pen, Scanner eyes) throws Exception{
+    pen.printf("Source (return for deposit): ");
+    String source = eyes.nextLine();
+
+    pen.printf("Target: ");
+    String recepient = eyes.nextLine();
+
+    pen.printf("Amount: ");
+    String amount = eyes.nextLine();
+    int val;
+    try {
+      val = Integer.valueOf(amount);
+      if (val <= 0) {
+        pen.println("please make sure your amnt is a positive nonzero value");
+        throw new Exception();
+      } // if
+    } catch (Exception e) {
+      pen.println("please make sure your amnt is a number");
+      throw new Exception();
+    } // try-catch
+
+    return (new Transaction(source, recepient, val));
+
+
+  }
 
 
   /**
@@ -47,22 +73,21 @@ public class BlockChainUI {
     BlockChain currentChain = new BlockChain(standardValidator);
 
     while (true) {
-      pen.printf("> ");
+      pen.printf("\n\nCommand: ");
       String commandLine = eyes.nextLine(); // Read user input
       if (commandLine.equals("quit")) {
         break;
       } else if (commandLine.equals("help")) {
         printInstructions(pen);
       } else if (commandLine.equals("balance")) {
-        pen.println("please enter the user you want to find");
-        pen.printf(">");
+        pen.printf("User: ");
         String findUser = eyes.nextLine(); // Read user input
         int balance = currentChain.balance(findUser);
         if (balance <= 0) {
           pen.printf("%s does not have a balance or does not exist,"
                 + "try users to see valid users", findUser);
         } else {
-          pen.printf("%s has a balance of %d\n", findUser, balance);
+          pen.printf("%s's balance is %d", findUser, balance);
         } // if-else
       } else if (commandLine.equals("users")) {
         Iterator<String> userIterator = currentChain.users();
@@ -71,69 +96,52 @@ public class BlockChainUI {
         } // while
       } else if (commandLine.equals("check")) {
         if (currentChain.isCorrect()) {
-          pen.println("the stored block chain is valid");
+          pen.printf("the stored block chain is valid");
         } else {
-          pen.println("there is an issue with the stored block chain.");
+          pen.printf("there is an issue with the stored block chain.");
         } // if-else
       } else if (commandLine.equals("remove")) {
         if (currentChain.removeLast()) {
-          pen.println("last element removed succesfully");
+          pen.printf("last element removed succesfully");
         } else {
-          pen.println("the array is empty so no elements where removed");
+          pen.printf("the array is empty so no elements where removed");
         } // if-else
       } else if (commandLine.equals("mine")) {
-        pen.println("Please enter the amount of the transaction");
-        pen.printf(">");
-        String amount = eyes.nextLine();
-        int val;
+        Transaction trans;
         try {
-          val = Integer.valueOf(amount);
-          if (val <= 0) {
-            pen.println("please make sure your amnt is a positive nonzero value");
-            continue;
-          } // if
+          trans = promptTransaction(pen, eyes);
         } catch (Exception e) {
-          pen.println("please make sure your amnt is a number");
           continue;
         } // try-catch
-        pen.println("Please enter the source user");
-        pen.printf(">");
-        String source = eyes.nextLine();
-        pen.println("Please enter the receiver");
-        pen.printf(">");
-        String recepient = eyes.nextLine();
-        Block newBlock = currentChain.mine(new Transaction(source, recepient, val));
-        pen.printf("your nonce is %d", newBlock.getNonce());
+
+        Block newBlock = currentChain.mine(trans);
+        pen.printf("\nUse nonce: %d", newBlock.getNonce());
+
       } else if (commandLine.equals("append")) {
-        pen.println("Please enter the ammount of the transaction");
-        pen.printf(">");
-        String amount = eyes.nextLine();
-        pen.println("Please enter the nonce");
-        pen.printf(">");
-        String nonceStr = eyes.nextLine();
-        int val;
-        int nonce;
+        Transaction trans;
         try {
-          val = Integer.valueOf(amount);
-          nonce = Integer.valueOf(nonceStr);
+          trans = promptTransaction(pen, eyes);
         } catch (Exception e) {
-          pen.println("please make sure your nonce and amount are numbers");
           continue;
         } // try-catch
-        pen.println("Please enter the source user");
-        pen.printf(">");
-        String source = eyes.nextLine();
-        pen.println("Please enter the reciever");
-        pen.printf(">");
-        String recepient = eyes.nextLine();
-        Transaction trans = new Transaction(source, recepient, val);
+
+        pen.printf("Nonce: ");
+        String nonceStr = eyes.nextLine();
+        long nonce;
+        try {
+          nonce = Long.valueOf(nonceStr);
+        } catch (Exception e) {
+          pen.println("please make sure your nonce is a number");
+          continue;
+        } // try-catch
         Block lastBlock = currentChain.getLastBlock();
         Block blk = new Block(lastBlock.getNum() + 1, trans, lastBlock.getHash(), nonce);
         try {
           currentChain.append(blk);
-          pen.printf("succesfully added block");
+          // + blk.toString()
+          pen.printf("Appended: " + blk.toString());
         } catch (Exception e) {
-          pen.printf("failed to add block because %e\n", e);
+          pen.printf("failed to add block because " +  e.getMessage());
         } // try-catch
       } else {
         pen.println("invalid command, use help to see the valid commands");
